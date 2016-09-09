52e0dc266dd8c4379828987d0162ccae1cb08f64
Benjamin Borbe <bborbe@rocketnews.de>
2016-09-09 15:55:23 +0200

    fix test

--- bin/postgres_backup_cron/postgres_backup_cron_test.go
+++ bin/postgres_backup_cron/postgres_backup_cron_test.go
@@ -3,16 +3,13 @@ package main
 import (
 	"testing"
 
-	"bytes"
-
 	"time"
 
 	. "github.com/bborbe/assert"
 )
 
 func TestDoFail(t *testing.T) {
-	writer := bytes.NewBufferString("")
-	err := do(writer, func(host string, port int, user string, pass string, database string, targetDir string) error {
+	err := do(func(host string, port int, user string, pass string, database string, targetDir string) error {
 		return nil
 	}, "", 0, "", "", "", "", time.Minute, false, "/tmp/lock")
 	if err := AssertThat(err, NotNilValue()); err != nil {
@@ -21,8 +18,7 @@ func TestDoFail(t *testing.T) {
 }
 
 func TestDoSuccess(t *testing.T) {
-	writer := bytes.NewBufferString("")
-	err := do(writer, func(host string, port int, user string, pass string, database string, targetDir string) error {
+	err := do(func(host string, port int, user string, pass string, database string, targetDir string) error {
 		return nil
 	}, "host", 5432, "user", "pass", "db", "/tmp", time.Minute, true, "/tmp/lock")
 	if err := AssertThat(err, NilValue()); err != nil {
